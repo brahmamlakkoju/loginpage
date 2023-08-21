3c033762506a41a38b74eef4fa8604e8f5924912
brahmamlakkoju <brahmamlakkoju@gmail.com>
2023-08-21 18:54:01 +0530

    loginpage

---
+++ src/About.js
@@ -0,0 +1,34 @@
+import React, { Component } from 'react'
+import './App.css'
+
+export default class About extends Component {
+  state={
+    productsList:[]
+  }
+  componentDidMount(){
+    fetch("https://backendjap.onrender.com/products")
+    .then((res)=>{
+      return res.json()
+    })
+    .then((resjson)=>{
+      this.setState({
+        productsList:resjson
+      })
+    })
+  }
+  render() {
+    return (
+      <div className='parent-product-item'>
+        {
+          this.state.productsList.map((product)=>(
+            <div className='product-item'>
+              <p>Name:{product.name}</p>
+              <p>Price:<b>{product.price}</b></p>
+              <img src={product.image} width="20%" alt={product.name}/>
+              </div>
+          ))
+        }
+      </div>
+    )
+  }
+}
